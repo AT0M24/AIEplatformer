4dff207de6cc5a1fcbe41dcd2176151e14cf688b
AT0M24 <ben_atkinson98@hotmail.com>
2015-09-28 18:44:07 +0930

    i did something

--- main.js
+++ main.js
@@ -7,6 +7,41 @@ var endFrameMillis = Date.now();
 var player = new Player();
 var keyboard = new Keyboard();
 
+var LAYER_COUNT = 2;
+var MAP = {tw:60, th:15};
+var TILE = 35;
+var TILESET_TILE = TILE * 2;
+var TILESET_PADDING = 2;
+var TILESET_SPACING = 2;
+var TILESET_COUNT_X = 14;
+var TILESET_COUNT_Y = 14;
+
+var tileset = document.createElement("img");
+tileset.src = "tileset.png";
+
+var cells = [];				// the array that holds our simplified collision data
+function initialize() {
+		for(var layerIdx = 0; layerIdx < LAYER_COUNT; layerIdx++) { //initialize the collision map
+		cells[layerIdx] = [];
+		var idx = 0;
+		for(var y = 0; y < level1.layers[layerIdx].height; y++) {
+			cells[layerIdx][y] = [];
+			for(var x = 0; x <level1.layers[layerIdx].width; x++) {
+				if(level1.layers[layerIdx].data[idx] != 0) {
+					// for each tile we find in the layer data, we need to create 4 collisions
+					// (because our collision squares are 35x35 but the tile in the level are 70x70)
+				cells[layerIdx][y][x] = 1;
+				cells[layerIdx][y-1][x] = 1;
+				cells[layerIdx][y-1][x+1] = 1;
+				cells[layerIdx][y][x+1] = 1; 
+				}
+				else if(cells[layerIdx][y][x] != 1) {
+					cells[layerIdx][y][x] = 0; 		// if we haven't set this cell's value, then set it to 0 now
+			}
+			idx++;
+		}
+	}
+}
 // This function will return the time in seconds since the function 
 // was last called
 // You should only call this function once per frame
@@ -44,12 +79,37 @@ var fpsCount = 0;
 var fpsTime = 0;
 
 // load an image to draw
-
+function drawMap()
+{
+ for(var layerIdx=0; layerIdx<LAYER_COUNT; layerIdx++)
+ {
+	var idx = 0;
+	for( var y = 0; y < level1.layers[layerIdx].height; y++ )
+	{
+		for( var x = 0; x < level1.layers[layerIdx].width; x++ )
+		{
+			if( level1.layers[layerIdx].data[idx] != 0 )
+			{
+			 // the tiles in the Tiled map are base 1 (meaning a value of 0 means no tile), so subtract one from the tileset id to get the
+			 // correct tile
+			 var tileIndex = level1.layers[layerIdx].data[idx] - 1;
+			 var sx = TILESET_PADDING + (tileIndex % TILESET_COUNT_X) * (TILESET_TILE + TILESET_SPACING);
+			 var sy = TILESET_PADDING + (Math.floor(tileIndex / TILESET_COUNT_Y)) * (TILESET_TILE + TILESET_SPACING);
+			 context.drawImage(tileset, sx, sy, TILESET_TILE, TILESET_TILE, x*TILE, (y-1)*TILE, TILESET_TILE, TILESET_TILE);
+			 }
+			 idx++;
+		}
+	}
+ }
+ 
+}
 
 function run()
 {
 	context.fillStyle = "#ccc";		
 	context.fillRect(0, 0, canvas.width, canvas.height);
+	
+	drawMap ();
 	
 	var deltaTime = getDeltaTime();
 	
@@ -70,9 +130,11 @@ function run()
 	context.fillStyle = "#f00";
 	context.font="14px Arial";
 	context.fillText("FPS: " + fps, 5, 20, 100);
+	
+	
 }
 
-
+initialize();
 //-------------------- Don't modify anything below here
 
 
