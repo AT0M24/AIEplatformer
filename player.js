b8f57fdfbf3423f2e1fbc5145cb4245797674aa1
AT0M24 <ben_atkinson98@hotmail.com>
2015-10-19 20:31:22 +1030

    started side scrolling

--- player.js
+++ player.js
@@ -25,7 +25,7 @@ var Player = function() {
 	}
 	
 	this.position = new Vector2();
-	this.position.set( 9*TILE, 0*TILE );
+	this.position.set( 4*TILE, 0*TILE );
 	
 	this.width = 159;
 	this.height = 163;
