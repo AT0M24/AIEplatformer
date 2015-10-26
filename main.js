5398da330f2dd5baa1c19e04babf88cd98bf8a4f
AT0M24 <ben_atkinson98@hotmail.com>
2015-10-26 20:23:54 +1030

    attempted to fix things but shits fucked

--- main.js
+++ main.js
@@ -4,10 +4,49 @@ var context = canvas.getContext("2d");
 var startFrameMillis = Date.now();
 var endFrameMillis = Date.now();
 
+
+
+// This function will return the time in seconds since the function 
+// was last called
+// You should only call this function once per frame
+function getDeltaTime()
+{
+	endFrameMillis = startFrameMillis;
+	startFrameMillis = Date.now();
+
+		// Find the delta time (dt) - the change in time since the last drawFrame
+		// We need to modify the delta time to something we can use.
+		// We want 1 to represent 1 second, so if the delta is in milliseconds
+		// we divide it by 1000 (or multiply by 0.001). This will make our 
+		// animations appear at the right speed, though we may need to use
+		// some large values to get objects movement and rotation correct
+	var deltaTime = (startFrameMillis - endFrameMillis) * 0.001;
+	
+		// validate that the delta is within range
+	if(deltaTime > 1)
+		deltaTime = 1;
+		
+	return deltaTime;
+};
+
+//-------------------- Don't modify anything above here
+
+var SCREEN_WIDTH = canvas.width;
+var SCREEN_HEIGHT = canvas.height;
+
+var ENEMY_MAXDX = METER * 5;
+var ENEMY_ACCEL = ENEMY_MAXDX * 2;
+
+var enemies = [];
+
 var LAYER_COUNT = 2;
 var LAYER_PLATFORMS = 0;
 var LAYER_LADDERS = 1;
-var MAP = {tw:60, th:15};
+
+var LAYER_OBJECT_ENEMIES = 2;
+var LAYER_OBJECT_TRIGGERS = 3;
+
+var MAP = {tw:100, th:16};
 var TILE = 35;
 var TILESET_TILE = TILE * 2;
 var TILESET_PADDING = 2;
@@ -70,6 +109,23 @@ function initialize() {
 		}
 	}
 	
+	idx = 0;
+	for(var y = 0; y < level1.layers[LAYER_OBJECT_ENEMIES].height; y++) 
+	{
+		for(var x = 0; x < level1.layers[LAYER_OBJECT_ENEMIES].width; x++) 
+		{
+			if(level1.layers[LAYER_OBJECT_ENEMIES].data[idx] != 0) 
+			{
+				var px = tileToPixel(x);
+				var py = tileToPixel(y);
+				var e = new Enemy(px, py);
+				enemies.push(e);
+			}
+			idx++;
+		}
+
+	} 
+	
 	musicBackground = new Howl(
 	{
 		urls: ["background.ogg"],
@@ -89,35 +145,6 @@ function initialize() {
 		}
 	} );
 }
-
-// This function will return the time in seconds since the function 
-// was last called
-// You should only call this function once per frame
-function getDeltaTime()
-{
-	endFrameMillis = startFrameMillis;
-	startFrameMillis = Date.now();
-
-		// Find the delta time (dt) - the change in time since the last drawFrame
-		// We need to modify the delta time to something we can use.
-		// We want 1 to represent 1 second, so if the delta is in milliseconds
-		// we divide it by 1000 (or multiply by 0.001). This will make our 
-		// animations appear at the right speed, though we may need to use
-		// some large values to get objects movement and rotation correct
-	var deltaTime = (startFrameMillis - endFrameMillis) * 0.001;
-	
-		// validate that the delta is within range
-	if(deltaTime > 1)
-		deltaTime = 1;
-		
-	return deltaTime;
-};
-
-//-------------------- Don't modify anything above here
-
-var SCREEN_WIDTH = canvas.width;
-var SCREEN_HEIGHT = canvas.height;
-
 
 // some variables to calculate the Frames Per Second (FPS - this tells use
 // how fast our game is running, and allows us to make the game run at a 
@@ -222,6 +249,7 @@ function run()
 	player.update(deltaTime);
 	player.draw();
 	drawMap ();
+	
 	// update the frame counter 
 	fpsTime += deltaTime;
 	fpsCount++;
@@ -231,6 +259,12 @@ function run()
 		fps = fpsCount;
 		fpsCount = 0;
 	}		
+	
+	for(var i=0; i<enemies.length; i++)
+	{
+		enemies[i].update(deltaTime);
+		enemies[i].draw();
+	}
 		
 	// draw the FPS
 	context.fillStyle = "#f00";
